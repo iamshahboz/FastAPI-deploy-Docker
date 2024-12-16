ebbd60bc2614cf026cb8da9fedded2eda1f57597
shahboz <sharafzodashahboz@gmail.com>
2024-12-16 14:20:47 +0500

    create tables added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from typing import List
-from fastapi import HTTPException, Depends, status, FastAPI
+from fastapi import Depends, status, FastAPI
 from sqlalchemy.orm import Session
 from . import models
 from . import schemas
 from fastapi import APIRouter
-from .database import get_db
+from .database import get_db, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
